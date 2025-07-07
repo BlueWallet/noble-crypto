6f2fd5c00a690c1064cd8733d315e00fc7eff0f8
overtorment <overtorment@gmail.com>
2025-07-07 09:38:27 +0100

    lint

--- noble-sign-wrapper.js
+++ noble-sign-wrapper.js
@@ -23,27 +23,33 @@ function toUint8Array(data) {
 function parseRSAKey(keyData) {
 	try {
 		var parsed = parseASN1(keyData);
-		
+
 		// Extract the raw values we need
 		var n = parsed.modulus;
 		var e = parsed.publicExponent;
 		var d = parsed.privateExponent;
-		
-		if (!n) throw new Error('Missing modulus');
-		if (!e) throw new Error('Missing exponent');
-		
+
+		if (!n) {
+			throw new Error('Missing modulus');
+		}
+		if (!e) {
+			throw new Error('Missing exponent');
+		}
+
 		// Convert to BigInt format expected by micro-rsa-dsa-dh
 		var nBig = BigInt('0x' + n.toString('hex'));
 		var eBig = BigInt('0x' + e.toString('hex'));
-		
+
 		if (d) {
 			// Private key
 			var dBig = BigInt('0x' + d.toString('hex'));
-			return { n: nBig, e: eBig, d: dBig };
-		} else {
-			// Public key
-			return { n: nBig, e: eBig };
+			return {
+				n: nBig, e: eBig, d: dBig
+			};
 		}
+		// Public key
+		return { n: nBig, e: eBig };
+
 	} catch (error) {
 		throw new Error('Failed to parse RSA key: ' + error.message);
 	}
@@ -55,8 +61,8 @@ var signers = {
 	'RSA-SHA256': rsa.PKCS1_SHA256,
 	'RSA-SHA384': rsa.PKCS1_SHA384,
 	'RSA-SHA512': rsa.PKCS1_SHA512,
-	'sha1WithRSAEncryption': rsa.PKCS1_SHA1,
-	'sha256WithRSAEncryption': rsa.PKCS1_SHA256
+	sha1WithRSAEncryption: rsa.PKCS1_SHA1,
+	sha256WithRSAEncryption: rsa.PKCS1_SHA256
 };
 
 function Sign(algorithm) {
@@ -73,7 +79,7 @@ Sign.prototype.update = function (data, encoding) {
 	if (this.finished) {
 		throw new Error('Cannot update after sign has been called');
 	}
-	
+
 	var uint8Data;
 	if (encoding) {
 		if (encoding === 'hex') {
@@ -86,7 +92,7 @@ Sign.prototype.update = function (data, encoding) {
 	} else {
 		uint8Data = toUint8Array(data);
 	}
-	
+
 	this.data.push(uint8Data);
 	return this;
 };
@@ -95,7 +101,7 @@ Sign.prototype.sign = function (privateKey, encoding) {
 	if (this.finished) {
 		throw new Error('Sign already called');
 	}
-	
+
 	// Concatenate all data
 	var totalLength = 0;
 	for (var i = 0; i < this.data.length; i++) {
@@ -108,7 +114,7 @@ Sign.prototype.sign = function (privateKey, encoding) {
 		concatenated.set(chunk, offset);
 		offset += chunk.length;
 	}
-	
+
 	// Parse private key
 	var keyData;
 	if (typeof privateKey === 'string' || privateKey instanceof Buffer) {
@@ -116,19 +122,19 @@ Sign.prototype.sign = function (privateKey, encoding) {
 	} else {
 		throw new Error('Unsupported private key format');
 	}
-	
+
 	// Sign the data
 	var signature = this.signer.sign(keyData, concatenated);
 	this.finished = true;
-	
+
 	// Return signature in requested format
 	if (encoding === 'hex') {
 		return Buffer.from(signature).toString('hex');
 	} else if (encoding === 'base64') {
 		return Buffer.from(signature).toString('base64');
-	} else {
-		return Buffer.from(signature);
 	}
+	return Buffer.from(signature);
+
 };
 
 function Verify(algorithm) {
@@ -145,7 +151,7 @@ Verify.prototype.update = function (data, encoding) {
 	if (this.finished) {
 		throw new Error('Cannot update after verify has been called');
 	}
-	
+
 	var uint8Data;
 	if (encoding) {
 		if (encoding === 'hex') {
@@ -158,7 +164,7 @@ Verify.prototype.update = function (data, encoding) {
 	} else {
 		uint8Data = toUint8Array(data);
 	}
-	
+
 	this.data.push(uint8Data);
 	return this;
 };
@@ -167,7 +173,7 @@ Verify.prototype.verify = function (publicKey, signature, encoding) {
 	if (this.finished) {
 		throw new Error('Verify already called');
 	}
-	
+
 	// Concatenate all data
 	var totalLength = 0;
 	for (var i = 0; i < this.data.length; i++) {
@@ -180,7 +186,7 @@ Verify.prototype.verify = function (publicKey, signature, encoding) {
 		concatenated.set(chunk, offset);
 		offset += chunk.length;
 	}
-	
+
 	// Parse public key
 	var keyData;
 	if (typeof publicKey === 'string' || publicKey instanceof Buffer) {
@@ -188,7 +194,7 @@ Verify.prototype.verify = function (publicKey, signature, encoding) {
 	} else {
 		throw new Error('Unsupported public key format');
 	}
-	
+
 	// Convert signature to Uint8Array
 	var sigData;
 	if (encoding === 'hex') {
@@ -198,11 +204,11 @@ Verify.prototype.verify = function (publicKey, signature, encoding) {
 	} else {
 		sigData = toUint8Array(signature);
 	}
-	
+
 	// Verify the signature
 	var result = this.signer.verify(keyData, concatenated, sigData);
 	this.finished = true;
-	
+
 	return result;
 };
 
@@ -219,4 +225,4 @@ module.exports = {
 	Sign: Sign,
 	createVerify: createVerify,
 	Verify: Verify
-}; 
+};
